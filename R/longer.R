5cd45fcd6578dfc1b8b2167492848285c8303de1
Ken Butler <ken@swiss-soccer.net>
2020-03-14 19:52:41 -0400

    make longer

---
+++ R/longer.R
@@ -0,0 +1,8 @@
+#' Make longer marks df
+#'
+#' @param marks_wide A wider df with one column per question and a column `Total` that is sum of marks for each student
+#' @return A longer df with all marks in one column and for each student and question: that student's total, the question number, the mark on that question
+#'
+make_long <- function(marks_wide) {
+  marks_wide %>% tidyr::pivot_longer(-Total, names_to="q", values_to="mark")
+}
